936421b7dff2e5e27a3e1895f3db92136be3395a
Ciurchil-Giurgiu Celia <celia.ciurchil@student.unitbv.ro>
2024-04-29 15:42:42 +0300

    i made the button work

--- Andorid_Tema_1/app/src/main/java/com/example/zoo/MainActivity.kt
+++ Andorid_Tema_1/app/src/main/java/com/example/zoo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.zoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.close_button)
+        button.setOnClickListener { goToSecondActivity() }
+    }
+
+    private fun goToSecondActivity() {
+        finish() // Close the current activity
     }
 }
